9942e77da77111ce7f515278b329787f040afbe7
darkars33 <darshangarad18@gmail.com>
2024-12-23 14:45:57 +0530

    fix the routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
-import { useState } from 'react'
-import {Routes, Route, Navigate} from 'react-router-dom'
-import Home from './pages/Home'
-import Saas from './pages/Saas'
-import SelfHosted from './pages/SelfHosted'
-import Error from './pages/Error'
-import { useUser } from '@clerk/clerk-react'
-import Navbar from './Component/Navbar'
+import { useState, useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Saas from './pages/Saas';
+import SelfHosted from './pages/SelfHosted';
+import Error from './pages/Error';
+import { useUser } from '@clerk/clerk-react';
+import Navbar from './Component/Navbar';
 
 function App() {
-  const {user} = useUser()
-  const login =  user ? true : false
- console.log(user)
-  
+  const { user, isSignedIn, isLoaded } = useUser();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (isLoaded) {
+      setIsAuthenticated(isSignedIn);
+    }
+  }, [isSignedIn, isLoaded]);
+
+  if (!isLoaded) {
+    
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className='flex'>
-    {login && <Navbar />}
+    <div className="flex">
+      {isAuthenticated && <Navbar />}
       <Routes>
-        <Route path='/' element={login ? <Home /> : <Navigate to="/saas"/>} />
-        <Route path='/saas' element={!login ? <Saas /> : <Navigate to="/" />} />
-        <Route path='/self-hosted' element={!login ? <SelfHosted /> : <Navigate to="/" />} />
-        <Route path='*' element={<Error />} />
+        <Route
+          path="/"
+          element={isAuthenticated ? <Home /> : <Navigate to="/saas" />}
+        />
+        <Route
+          path="/saas"
+          element={!isAuthenticated ? <Saas /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/self-hosted"
+          element={!isAuthenticated ? <SelfHosted /> : <Navigate to="/" />}
+        />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
